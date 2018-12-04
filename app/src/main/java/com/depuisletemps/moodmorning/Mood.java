55d9b3f54c00e9657d1390cc0fe1041207b44bf6
Bertrand <pafacil@gmail.com>
2018-12-04 16:31:18 +0100

    Beginning of the MoodMorning Application

---
+++ app/src/main/java/com/depuisletemps/moodmorning/Mood.java
@@ -0,0 +1,9 @@
+package com.depuisletemps.moodmorning;
+
+public enum Mood {
+    SAD("#ffde3c50", "smileySad"),
+    BORED("#ff9b9b9b", ),
+    REGULAR("#a5468ad9", ),
+    GLAD("#ffb8e986", ),
+    HAPPY("#fff9ec4f", );
+}
