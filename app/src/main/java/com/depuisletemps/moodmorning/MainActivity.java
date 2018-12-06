2cd2ee771b36cd6360987b9a21c951d26d1d91e9
Bertrand <pafacil@gmail.com>
2018-12-06 10:29:34 +0100

    Modified the layout and pictures directories

--- app/src/main/java/com/depuisletemps/moodmorning/MainActivity.java
+++ app/src/main/java/com/depuisletemps/moodmorning/MainActivity.java
@@ -2,12 +2,22 @@ package com.depuisletemps.moodmorning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mMood;
+    private Button mCommentBtn;
+    private Button mHistoryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMood = (ImageView) findViewById(R.id.activity_main_mood);
+        mCommentBtn = (Button) findViewById(R.id.activity_main_comment);
+        mHistoryBtn = (Button) findViewById(R.id.activity_main_history);
     }
 }
--- app/src/main/java/com/depuisletemps/moodmorning/Mood.java
+++ app/src/main/java/com/depuisletemps/moodmorning/Mood.java
@@ -1,9 +1,25 @@
 package com.depuisletemps.moodmorning;
 
 public enum Mood {
-    SAD("#ffde3c50", "smileySad"),
-    BORED("#ff9b9b9b", ),
-    REGULAR("#a5468ad9", ),
-    GLAD("#ffb8e986", ),
-    HAPPY("#fff9ec4f", );
+    SAD("#ffde3c50", "smiley-sad"),
+    BORED("#ff9b9b9b", "smiley-bored"),
+    REGULAR("#a5468ad9", "smiley-regular"),
+    GLAD("#ffb8e986", "smiley-glad"),
+    HAPPY("#fff9ec4f", "smiley-happy");
+
+    private String color;
+    private String fileName;
+
+    Mood(String color, String fileName){
+        this.color = color;
+        this.fileName = fileName;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
 }
