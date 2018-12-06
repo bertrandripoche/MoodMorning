e81e01c5a3735314237e8c680858935959435699
Bertrand <pafacil@gmail.com>
2018-12-06 18:17:01 +0100

    Modified Button for ImageButton instead

--- app/src/main/java/com/depuisletemps/moodmorning/MainActivity.java
+++ app/src/main/java/com/depuisletemps/moodmorning/MainActivity.java
@@ -4,15 +4,17 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView mMood;
-    private Button mCommentBtn;
-    private Button mHistoryBtn;
+    private ImageButton mCommentBtn;
+    private ImageButton mHistoryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,26 +22,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mMood = (ImageView) findViewById(R.id.activity_main_mood);
-        mCommentBtn = (Button) findViewById(R.id.activity_main_comment);
-        mHistoryBtn = (Button) findViewById(R.id.activity_main_history);
+        mCommentBtn = (ImageButton) findViewById(R.id.activity_main_comment);
+        mHistoryBtn = (ImageButton) findViewById(R.id.activity_main_history);
 
-        mCommentBtn.setOnClickListener(
-            // method to display an alert dialog with an edit text
-                final EditText commentBox = new EditText(this);
+        mCommentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+ /*               final EditText commentBox = new EditText();
 
-            AlertDialog dialog = new AlertDialog.Builder(this)
-                    .setTitle("About my mood")
-                    .setMessage("Add a comment")
-                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            String comment = String.valueOf(commentBox.getText());
-                        }
-                    })
-                    .setNegativeButton("Cancel", null)
-                    .create();
-        );
-
-
+                AlertDialog dialog = new AlertDialog.Builder()
+                        .setTitle("About my mood")
+                        .setMessage("Add a comment")
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String comment = String.valueOf(commentBox.getText());
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();*/
+ System.out.print("TEST OK");
+            }
+       });
     }
 }
