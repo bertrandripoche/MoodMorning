362fb4ecbbb6f2ba455bc7d53497ecf6b2cd3c6e
Bertrand <pafacil@gmail.com>
2018-12-06 11:33:11 +0100

    Finally fixed the image issue - name were containing a dash then a capital letter, not possible in resource directory

--- app/src/main/java/com/depuisletemps/moodmorning/MainActivity.java
+++ app/src/main/java/com/depuisletemps/moodmorning/MainActivity.java
@@ -1,8 +1,11 @@
 package com.depuisletemps.moodmorning;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
         mMood = (ImageView) findViewById(R.id.activity_main_mood);
         mCommentBtn = (Button) findViewById(R.id.activity_main_comment);
         mHistoryBtn = (Button) findViewById(R.id.activity_main_history);
+
+        mCommentBtn.setOnClickListener(
+            // method to display an alert dialog with an edit text
+                final EditText commentBox = new EditText(this);
+
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("About my mood")
+                    .setMessage("Add a comment")
+                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String comment = String.valueOf(commentBox.getText());
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .create();
+        );
+
+
     }
 }
