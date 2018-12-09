c720be974da99ed77494b674fa291a963d56ba28
Bertrand <pafacil@gmail.com>
2018-12-09 18:34:02 +0100

    Started to add Shared Preferences in the Comment AlertDialog window, started the layout for the future History Activity

---
+++ app/src/main/java/com/depuisletemps/moodmorning/History.java
@@ -0,0 +1,6 @@
+package com.depuisletemps.moodmorning;
+
+import android.app.Activity;
+
+public class History extends Activity {
+}
---
+++ app/src/main/java/com/depuisletemps/moodmorning/MoodStore.java
@@ -0,0 +1,7 @@
+package com.depuisletemps.moodmorning;
+
+import android.content.SharedPreferences;
+
+public class MoodStore {
+    private SharedPreferences mPreferences;
+}
